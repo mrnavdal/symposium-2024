38b55f26bc2fb14e67f085dac2eb77310196ddc8
Dalibor Mrnavek <dalibor.mrnavek@gmail.com>
2024-11-10 20:46:56 +0100

    Locally built

--- symposium24/.history/src/components/ui/container_20241110204512.tsx
+++ symposium24/.history/src/components/ui/container_20241110204512.tsx
@@ -3,7 +3,6 @@ import { cn } from '@/lib/utils'
 interface ContainerProps {
   className?: string;
   children?: React.ReactNode;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   [key: string]: any;
 }
 
