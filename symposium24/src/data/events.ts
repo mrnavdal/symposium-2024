7b1d3d146c455f091529d8ba065d0c0ca7e698f8
Dalibor Mrnavek <dalibor.mrnavek@gmail.com>
2024-11-21 20:19:28 +0100

    Update mista prohlídky

--- symposium24/src/data/events.ts
+++ symposium24/src/data/events.ts
@@ -158,7 +158,7 @@ export const events: CalendarEvent[] = [
     start: new Date(2024, new Date().getMonth(), new Date().getDate(), 19, 10),
     end: new Date(2024, new Date().getMonth(), new Date().getDate(), 19, 40),
     speaker: 'Eliška Malachová',
-    location: 'Pod schody do druhého patra',
+    location: 'Sraz před schody do nejnižšího patra',
     description: 'Kromě přednášek a workshopů se také můžete těšit na jedinečnou CLUBCO TOUR a nechat se provést prostory Clubco ve Vlněně. 😍 Pokud jste začínající podnikatelé nebo prostě jen hledáte inspiraci pro své podnikatelské ambice, neměli byste si rozhodně nechat ujít tuto jedinečnou příležitost v rámci konference Symposium. Večer od 20:10 se můžete připojit ke komentované prohlídce prostor Clubco ve Vlněně, kterou povede Event and community associate Eliška Malachová. \n\nNa programu bude:\n🔸 Historie Clubco - jak vše začalo až po současnost.\n🔸 Nahlédnutí do moderního interiéru, který může být inspirací pro váš pracovní prostor.\n🔸 Výhody členství v coworkingovém prostoru - kultura, networking a podpora, kterou potřebujete. \n🔸 Podcastové studio a další novinky.\n\nEliška Malachová vás provede úžasným prostorem Clubco ve Vlněně. Těšíme se na vás! 🚀'
 },
 ]
